bf20f605cf0fe43ac1570c529d6312f9136d7835
Ankit Kesharwani <kesharwaniankit80@gmail.com>
2025-02-19 20:42:54 +0530

    improved disclaimer font size

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="text-md text-gray-400 p-4 bg-gray-800 border-t border-gray-700 text-center">
+      <footer className="text-xs text-gray-400 p-4 bg-gray-800 border-t border-gray-700 text-center">
         Disclaimer: The simulated data provides an approximate financial projection to help you gain a holistic understanding of how various factors may impact your financial future. It should not be considered financial advice.
       </footer>
     );
