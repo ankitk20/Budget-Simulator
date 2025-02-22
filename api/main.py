1478ac407e095cf0bd555d931c50c7bfad219f56
Ankit Kesharwani <kesharwaniankit80@gmail.com>
2025-02-22 19:35:11 +0530

    enhancement to port fetch

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from slowapi.middleware import SlowAPIMiddleware
 from middleware import limiter
@@ -15,7 +16,8 @@ app.include_router(simulate_router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
 
 # TODO: handle api response where things are -ve
 # TODO: Downpayment adjust is pending
