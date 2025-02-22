d25b25b365a22c157d7e523df61e4fd9634fab5b
Ankit Kesharwani <kesharwaniankit80@gmail.com>
2025-02-22 23:10:01 +0530

    added cors policy

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI
 from slowapi.middleware import SlowAPIMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from middleware import limiter
 from routes.simulate import router as simulate_router
 
@@ -9,7 +10,16 @@ app = FastAPI()
 
 # Attach rate limiter to app state
 app.state.limiter = limiter
-app.add_middleware(SlowAPIMiddleware)
+
+# CORS configuration with stricter settings
+app.add_middleware(
+    SlowAPIMiddleware,
+    CORSMiddleware,
+    allow_origins=["https://fisim.vercel.app/", "http://192.168.0.225:3000"],  # Allow specific origin
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],  # Allow only POST requests
+    allow_headers=["*"],  # Allow specific headers
+)
 
 # Register Routes
 app.include_router(simulate_router, prefix="/api")
