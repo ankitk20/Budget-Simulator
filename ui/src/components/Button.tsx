1436bc37790b54b841e9a04659d435feab282e5c
Ankit Kesharwani <kesharwaniankit80@gmail.com>
2025-02-27 18:16:53 +0530

    made button component accept classes

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -1,20 +1,18 @@
 interface ButtonProps {
-    label: string;
-    onClick: () => void;
-    loading?: boolean;
-  }
-  
-  export default function Button({ label, onClick, loading = false }: ButtonProps) {
-    return (
-      <button
-        onClick={onClick}
-        disabled={loading}
-        className={`text-white px-4 py-2 rounded-lg font-semibold transition-all duration-200 bg-gray-700 text-gray-200 
-          ${loading ? "cursor-not-allowed opacity-50" : "hover:bg-gray-600"}
-        `}
-      >
-        {label}
-      </button>
-    );
-  }
-  
+  label: string;
+  onClick: () => void;
+  loading?: boolean;
+  className?: string;
+}
+
+export default function Button({ label, onClick, loading = false, className = "" }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={loading}
+      className={`${loading ? "cursor-not-allowed opacity-50" : "hover:bg-gray-600"} ${className}`}
+    >
+      {label}
+    </button>
+  );
+}
