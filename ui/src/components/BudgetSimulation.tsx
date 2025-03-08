daef553586aba6ad6632400c5ca683d523b690a4
Ankit Kesharwani <kesharwaniankit80@gmail.com>
2025-03-08 18:32:39 +0530

    scoped get country api call on country change only

--- ui/src/components/BudgetSimulation.tsx
+++ ui/src/components/BudgetSimulation.tsx
@@ -109,7 +109,7 @@ export default function BudgetSimulation({ demo = true }: BudgetSimulationProps)
     }
   
     fetchUserInput();
-  }, [formData, session]); // Ensure dependencies are correctly listed
+  }, [formData.country, session]); // Ensure dependencies are correctly listed
   
   const addInvExpense = async (tableData: TableData[]) => {
     editDataRef.current = await addInvExpenseEntry(tableData);
